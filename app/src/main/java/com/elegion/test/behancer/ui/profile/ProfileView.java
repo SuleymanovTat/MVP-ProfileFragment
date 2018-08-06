cbeedf426a9cb82427cf447f662f2157c8c9a07d
suleymanovtat <suleymanovtat@gmail.com>
2018-08-06 10:05:16 +0300

    MVP в ProfileFragment

---
+++ app/src/main/java/com/elegion/test/behancer/ui/profile/ProfileView.java
@@ -0,0 +1,15 @@
+package com.elegion.test.behancer.ui.profile;
+
+import android.support.annotation.NonNull;
+
+import com.elegion.test.behancer.common.BaseView;
+import com.elegion.test.behancer.data.model.user.User;
+
+/**
+ * Created by Vladislav Falzan.
+ */
+
+public interface ProfileView extends BaseView {
+
+    void showProfile(@NonNull User user);
+}
